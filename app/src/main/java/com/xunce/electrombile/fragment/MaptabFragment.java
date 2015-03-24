6cddd3480e5def34ef5860371ee97c2768c5cdb0
heyukun <826319028@qq.com>
2015-03-24 21:43:20 +0800

    add baidumap view to mapTabFragment

--- app/src/main/java/com/xunce/electrombile/fragment/MaptabFragment.java
+++ app/src/main/java/com/xunce/electrombile/fragment/MaptabFragment.java
@@ -8,20 +8,54 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 import com.xunce.electrombile.R;
 
 public class MaptabFragment extends Fragment {
 
+    MapView mMapView = null;
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(this.getActivity().getApplicationContext());
+    }
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.map_fragment, container, false);
-	}
-	
+
+		View view = inflater.inflate(R.layout.map_fragment, container, false);
+        mMapView = (MapView) view.findViewById(R.id.bmapView);
+        return view;
+    }
+
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
 		((TextView)getView().findViewById(R.id.tvTop)).setText("地图");
 	}
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
 }
